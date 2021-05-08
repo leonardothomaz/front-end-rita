442afffebe4eef646fa8ae16d2a68cdeb4cfc685
José Otávio Bremm <bremm@alunos.utfpr.edu.br>
2021-05-08 13:06:02 -0300

    Corrigindo landingpage em produção
    deu ruim o conflito, corrigi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import { Body } from '../components/Body'
-// import { Cadastro } from '../components/Cadastro'
 
 export default function Home() {
-  return (
-    
-    <body>
-      <Header />
-      <Body />
-      {/* <Footer /> */}
-    </body>
-  )
+  return <Body />
 }
